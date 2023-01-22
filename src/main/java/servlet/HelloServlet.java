94ec58fd94161f8d29e230ef2e0dd8d0be9943e7
Vamsee Achanta <vamsee.achanta@aceengineer.com>
2023-01-22 07:40:00 -0600

    servlet not working

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -9,10 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(
-        name = "MyServlet",
-        urlPatterns = {"/hello"}
-    )
+@WebServlet(name = "MyServlet", urlPatterns = { "/hello" })
 public class HelloServlet extends HttpServlet {
 
     @Override
---
+++ src/main/java/servlet/HttpsEnforcer.java
@@ -0,0 +1,44 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.FilterChain;
+
+public class HttpsEnforcer implements Filter {
+    public static final String X_FORWARDED_PROTO = "X-Forwarded-Proto";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        if (request.getHeader(X_FORWARDED_PROTO) != null) {
+            if (request.getHeader(X_FORWARDED_PROTO).indexOf("https") != 0) {
+                String pathInfo = (request.getPathInfo() != null) ? request.getPathInfo() : "";
+                response.sendRedirect("https://" + request.getServerName() + pathInfo);
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
